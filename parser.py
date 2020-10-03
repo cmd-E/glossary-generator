3174ff0684e089475682ac195632f4e36bdc3e99
DEUS <perevyshink_1@mail.ru>
2020-10-03 23:34:08 +0600

    added random ability

--- parser.py
+++ parser.py
@@ -1,17 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+import random
 url = "http://libr.aues.kz/facultet/frts/kaf_aes/52/umm/aes_1.htm"
 
+termsCount = int(input("Колличество терминов: "))
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
 trs = soup.find("table").find_all("tr")
+trs = trs[1:]
 i = 0
-file = open("glossary.txt", "w", encoding="UTF-8")
 description = ""
 term = ""
-for tr in trs[1:]:
+termsList = []
+for tr in trs:
     if i % 2 != 0:
         description = tr.find("td").find("p").getText()
         description = description.replace("\r", "")
@@ -24,9 +26,20 @@ for tr in trs[1:]:
         i += 1
         continue
     else:
-        line = f"{term} - {description}\r"
-        file.write(line)
+        # line = f"{term} - {description}\r"
+        termsList.append([
+            term, description
+        ])
         term = ""
         description = ""
+used = []
+glossaryFile = open("glossary.txt", "w", encoding="UTF-8")
+i = 0
+while i < termsCount:
+    randomTerm = random.randint(0, len(termsList))
+    if randomTerm not in used:
+        term = termsList[randomTerm]
+        glossaryFile.write(f"{term[0]} - {term[1]}\n")
+        used.append(randomTerm)
         i += 1
-file.close()
+glossaryFile.close()
