9042c255374cd1b844864c27ff7fb851eb723244
Kirill <perevyshink_1@mail.ru>
2021-04-29 10:20:19 +0600

    Add progressbar for terms downloading If terms have already been downloaded, read from file Add tqdm to requirements.txt

--- terms_loader.py
+++ terms_loader.py
@@ -1,10 +1,10 @@
+import os.path
 import random
 import sys
-
 import requests
 from bs4 import BeautifulSoup
-
 from app_config import AppConfig
+from tqdm import tqdm
 
 
 class TermsLoader:
@@ -15,6 +15,41 @@ class TermsLoader:
         terms_list = cls.__parse_terms(raw_terms, AppConfig.lang)
         selected_terms = cls.__get_random_terms(terms_list)
         return selected_terms
+
+    @staticmethod
+    def __load_raw_terms() -> list:
+        url = "http://libr.aues.kz/facultet/frts/kaf_aes/52/umm/aes_1.htm"
+        html_filename = 'terms_origin.html'
+        if not os.path.isfile(html_filename):
+            print('Загрузка терминов...')
+            try:
+                response = requests.get(url, stream=True)
+                total_size_in_bytes = int(response.headers.get('content-length', 0))
+                block_size = 1024  # 1 Kibibyte
+                progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+                with open(html_filename, 'wb') as file:
+                    for data in response.iter_content(block_size):
+                        progress_bar.update(len(data))
+                        file.write(data)
+                progress_bar.close()
+                if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+                    print("ERROR, something went wrong")
+            except:
+                print("Unexpected error occurred while getting response from glossary page: ",
+                      sys.exc_info()[0])
+                exit()
+        soup = ""
+        try:
+            with open(html_filename, 'r', encoding="utf-8") as file:
+                content = file.read()
+            soup = BeautifulSoup(content, "html.parser")
+        except:
+            print("Unexpected error occurred while parsing response from glossary page: ",
+                  sys.exc_info()[0])
+        print("Парсинг терминов...")
+        trs = soup.find("table").find_all("tr")
+        trs = trs[1:]
+        return trs
 
     @classmethod
     def __parse_terms(cls, raw_terms: list, lang: str) -> list:
@@ -67,25 +102,3 @@ class TermsLoader:
                 AppConfig.terms_count += 1
                 continue
         return selected_terms
-
-    @staticmethod
-    def __load_raw_terms() -> list:
-        url = "http://libr.aues.kz/facultet/frts/kaf_aes/52/umm/aes_1.htm"
-        response = ""
-        try:
-            response = requests.get(url)
-        except:
-            print("Unexpected error occurred while getting response from glossary page: ",
-                  sys.exc_info()[0])
-            exit()
-
-        soup = ""
-        try:
-            soup = BeautifulSoup(response.content, "html.parser")
-        except:
-            print("Unexpected error occurred while parsing response from glossary page: ",
-                  sys.exc_info()[0])
-
-        trs = soup.find("table").find_all("tr")
-        trs = trs[1:]
-        return trs
